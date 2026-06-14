05f4f53530637fb4a742edb407b7a9ce2c460a37
ricardoquintela <nortefortequintela@gmail.com>
2026-06-14 12:24:54 +0100

    Create invite.js

---
+++ api/invite.js
@@ -0,0 +1,6 @@
+api/
+  auth.js          ← já existe
+  login.js         ← já existe
+  reset-password.js ← já existe
+  notify-fight-result.js ← já existe
+  invite.js        ← novo, adicionar aqui
